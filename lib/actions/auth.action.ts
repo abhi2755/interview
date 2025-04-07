55378b4525fb2edc7c884ed14bf7cbd184452d91
Abhishek <abhi279555@gmail.com>
2025-04-08 01:13:47 +0530

    ai interview ui implemented

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -3,7 +3,6 @@
 import { auth, db } from "@/firebase/admin";
 import { cookies } from "next/headers";
 
-
 const ONE_WEEK = 60 * 60 * 24 * 7;
 
 export async function signUp(params: SignUpParams) {
@@ -22,6 +21,11 @@ export async function signUp(params: SignUpParams) {
       name,
       email,
     });
+
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in",
+    };
   } catch (e: any) {
     console.error("Error creating an user", e);
     if (e.code === "auth/email-already-exists") {
@@ -38,26 +42,26 @@ export async function signUp(params: SignUpParams) {
 }
 
 export async function signIn(params: SignInParams) {
-    const { email, idToken } = params;
-  
-    try {
-      const userRecord = await auth.getUserByEmail(email);
-      if (!userRecord)
-        return {
-          success: false,
-          message: "User does not exist. Create an account.",
-        };
-  
-      await setSessionCookie(idToken);
-    } catch (error: any) {
-      console.log(error);
-  
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord)
       return {
         success: false,
-        message: "Failed to log into account. Please try again.",
+        message: "User does not exist. Create an account.",
       };
-    }
+
+    await setSessionCookie(idToken);
+  } catch (error: any) {
+    console.log(error);
+
+    return {
+      success: false,
+      message: "Failed to log into account. Please try again.",
+    };
   }
+}
 
 export async function setSessionCookie(idToken: string) {
   const cookieStore = await cookies();
@@ -71,6 +75,33 @@ export async function setSessionCookie(idToken: string) {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     path: "/",
-    sameSite: "lax"
+    sameSite: "lax",
   });
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+  return !!user;
 }
